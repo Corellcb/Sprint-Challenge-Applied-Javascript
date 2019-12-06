483815d349603d229d4fc18b52d321ac4e8e0b51
CorellCB <corellcb@gmail.com>
2019-12-06 14:05:58 -0500

    completed part of stretch

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -6,6 +6,7 @@
     5. Think of how you would animate this component. Make the cards slide in and out, or fade. It's up to you!
     6. Have fun!
 */
+const carouselEntry = document.querySelector('.carousel-container');
 
 /* HTML:
   <div class="carousel">
@@ -17,3 +18,84 @@
     <div class="right-button"> > </div>
   </div>
 */
+counter = 1;
+function createCarousel() {
+  const carousel = document.createElement('div'),
+  leftBtn = document.createElement('div'),
+  img1 = document.createElement('img'),
+  img2 = document.createElement('img'),
+  img3 = document.createElement('img'),
+  img4 = document.createElement('img'),
+  rightBtn = document.createElement('div');
+
+  carousel.classList.add('carousel');
+  leftBtn.classList.add('left-button');
+  rightBtn.classList.add('right-button');
+
+  img1.setAttribute('src', "../assets/carousel/mountains.jpeg");
+  img1.style.display = 'inline-block';
+  img2.setAttribute('src', "../assets/carousel/computer.jpeg");
+  img3.setAttribute('src', "../assets/carousel/trees.jpeg");
+  img4.setAttribute('src', "../assets/carousel/turntable.jpeg");
+
+  leftBtn.textContent = "\u2B9C";
+  rightBtn.textContent = "\u2B9E";
+
+  carousel.appendChild(leftBtn);
+  carousel.appendChild(img1);
+  carousel.appendChild(img2);
+  carousel.appendChild(img3);
+  carousel.appendChild(img4);
+  carousel.appendChild(rightBtn);
+
+  leftBtn.addEventListener('click', (event) => {
+    if(counter === 1){
+      img1.style.display = 'none';
+      img4.style.display = 'inline-block';
+      counter = 4;
+    } 
+    else if (counter === 2) {
+      img2.style.display = 'none';
+      img1.style.display = 'inline-block';
+      counter = 1;
+    } 
+    else if (counter === 3) {
+      img3.style.display = 'none';
+      img2.style.display = 'inline-block';
+      counter = 2;
+    }
+    else if (counter === 4) {
+      img4.style.display = 'none';
+      img3.style.display = 'inline-block';
+      counter = 3;
+    }
+  })
+
+  rightBtn.addEventListener('click', (event) => {
+    if(counter === 1){
+      img1.style.display = 'none';
+      img2.style.display = 'inline-block';
+      counter = 2;
+    } 
+    else if (counter === 2) {
+      img2.style.display = 'none';
+      img3.style.display = 'inline-block';
+      counter = 3;
+    } 
+    else if (counter === 3) {
+      img3.style.display = 'none';
+      img4.style.display = 'inline-block';
+      counter = 4;
+    }
+    else if (counter === 4) {
+      img4.style.display = 'none';
+      img1.style.display = 'inline-block';
+      counter = 1;
+    }
+  })
+
+  carouselEntry.appendChild(carousel);
+}
+
+createCarousel();
+
